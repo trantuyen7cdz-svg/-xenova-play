9c3e47c36e1bbbae206d70b34c57167de5faafca
trantuyen7cdz-svg <trantuyen7cdz@gmail.com>
2026-09-18 19:21:38 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import Menu from "./components/Menu";
+import ChatButton from "./components/ChatButton";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <body>
         <Menu />
+        <ChatButton />
         {children}
       </body>
     </html>
